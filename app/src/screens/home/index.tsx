a83800fa9f8f700f2c4d367b8bb0a78f16b0d454
pedro <pedromiguelnevesdemeu@gmail.com>
2021-10-24 12:00:30 -0300

    My first stylesheet

--- app/src/screens/home/index.tsx
+++ app/src/screens/home/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-
+import {styles} from './styles'
 export function Home() {
 
     return (
-        <View>
+        <View  style={styles.container}>
             <Text>Hello Worlds! I'm programming to mobile apps </Text>
         </View>
     )
