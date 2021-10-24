e33231e46d6108b4597b66f6088838e66a9a43ab
pedro <pedromiguelnevesdemeu@gmail.com>
2021-10-24 12:29:55 -0300

    adding customer fonts

--- app/App.tsx
+++ app/App.tsx
@@ -1,9 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {Home} from './src/screens/home';
+import {
+  useFonts,
+
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto'
+import AppLoading from 'expo-app-loading';
+
+import { Home } from './src/screens/home';
+
 export default function App() {
-  return (
-    <Home />
-  );
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+  if (!fontsLoaded) {
+    return (<AppLoading />)
+  } else {
+    return (<Home />)
+  }
 }
